dcff11f80fa3a8e914e5be5e5afd8b1f061cdd0e
Victor <91267367+victoraldebert@users.noreply.github.com>
2022-05-03 15:33:20 +0200

    Adding the different shop pages

---
+++ scroll.js
@@ -0,0 +1,49 @@
+const sr = ScrollReveal();                  //Méthode de l'effet
+
+sr.reveal('.first', {
+    origin: 'left',
+    distance: '50px',
+    scale: 0.95,
+    reset: true,
+    duration: 2000  
+});    
+
+sr.reveal('.second', {
+    origin: 'right',
+    distance: '50px',
+    scale: 0.95,
+    reset: true,
+    duration: 2000  
+})
+sr.reveal('.third', {
+    origin: 'left',
+    distance: '50px',
+    scale: 0.95,
+    reset: true,
+    duration: 2000  
+});    
+
+sr.reveal('.fourth', {
+    origin: 'right',
+    distance: '50px',
+    scale: 0.95,
+    reset: true,
+    duration: 2000  
+});    
+
+sr.reveal('.fifth', {
+    origin: 'bottom',
+    distance: '50px',
+    scale: 0.95,
+    reset: true,
+    duration: 2500 
+});    
+
+sr.reveal('.sixth', {
+    origin: 'bottom',
+    distance: '50px',
+    scale: 0.95,
+    reset: true,
+    duration: 2500
+});    
+
